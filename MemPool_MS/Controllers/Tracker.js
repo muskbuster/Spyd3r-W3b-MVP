984ac0dbdf1aa8538101bb2c0ea363f686e4c879
muskbuster <sudeepkamat79@gmail.com>
2023-04-12 11:31:08 +0000

    code completion(document remaining)

--- MemPool_MS/Controllers/Tracker.js
+++ MemPool_MS/Controllers/Tracker.js
@@ -24,16 +24,21 @@ const trackTransactions = async () => {
       const Signer = new ethers.Wallet(process.env.PRIVATE_KEY, MUMBAI_80001);
     //we should check that the transaction is confirmed within 10 blocks
     QuickNode.on("pending", async (txHash) => {
+      //put a transaction in dynamic json object and if it is called again within 20 blocks the we pause the contract
+      Monitoring={};
+      EthertransferBlock={};
       try {
         //console.log("pending txHash: ",txHash);
         // Wait for the transaction to be mined
         const receipt = await QuickNode.getTransaction(txHash);
+        const blockNumber = await QuickNode.getBlockNumber();
         //filter out the transactions that are not related to our contract
-        if (receipt.to === contractAddress) {
+        if (receipt.to === abi.address ) {
           console.log("receipt: ",receipt);
           //check if the transaction is a stake or unstake
           const tx = await QuickNode.getTransaction(txHash);
           const data = tx.data;
+          const ethValue = tx.value;
           console.log("data: ",data);
 // now we run the transaction through ethereumjs-vm with decoded data with function name and amount
           const txData = { from: tx.from, to: tx.to, data: data };
@@ -43,6 +48,13 @@ const trackTransactions = async () => {
           console.log("txDatadata : ",txData.data);
 
           //To check gas fee needed then check if it the gas sent is lower than the gas needed and broadcast as kafka producer as topic-test
+          const LookForStake ="0x3a4b66f1";
+          const LookForUnstake ="0x2def6620";
+          const functionCall=data.slice(0,10);
+          if(functionCall===LookForStake || functionCall===LookForUnstake)
+          {
+            console.log("functionCall: ",functionCall);
+            console.log("trying for low gas ");
           const gasNeeded = await QuickNode.estimateGas(txData);
           console.log("gasNeeded: ",gasNeeded);
           const gasSent = tx.gasLimit;
@@ -50,13 +62,38 @@ const trackTransactions = async () => {
           if (gasSent < gasNeeded) {
             console.log("gasSent is less than gasNeeded");
             //call pause function to pause the contract
-            const pause = await contract.connect(Signer).pause();
+            const pause = await contract.connect(Signer).pause({ gasLimit: 510750, gasPrice: gas + BigInt(100000000000) });
+            console.log("pause: ",pause);
           }
-else if (receipt.to !== contractAddress){
- //we ignore the transaction
- console.log("");
-}
+          console.log("trying to monitor repeated calls");
+          if( Monitoring[txData.from] - blockNumber > 20 )
+          {
+            const pause = await contract.connect(Signer).pause({ gasLimit: 510750, gasPrice: gas + BigInt(100000000000) });
+            console.log("pause: ",pause);
+          }
+          Monitoring[txData.from]=blockNumber;
+          console.log("Monitoring: ",Monitoring);
+          console.log("trying for ether transfers happening")
+          //monitor ether transfers that is happening from contract outwards and if it is going to same address more than 3 times in 20 blocks then pause the contract
+          if(ethValue!=0 && EthertransferBlock[txData.from] - blockNumber > 20 )
+          {
+            const pause = await contract.connect(Signer).pause({ gasLimit: 510750, gasPrice: gas + BigInt(100000000000) });
+            console.log("pause: ",pause);
+          }
+          EthertransferBlock[txData.from]=blockNumber;
+
+          if(ethValue==0)
+          {
+            console.log("ethValue is zero");
+          }
+
+
  }
+ else if (receipt.to !== abi.address){
+  //we ignore the transaction
+  console.log("");
+ }
+}
 
  } catch (err) {
         console.error(`Error handling pending transaction ${txHash}:`, err);
--- README.md
+++ README.md
@@ -11,3 +11,6 @@ To run this application:
 ```
 npm start
 ```
+Kafka start
+```
+export HOST_IP=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1) 
